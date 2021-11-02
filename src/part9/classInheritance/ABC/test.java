4aba5d2262aa3d32516185e64847a2bcca09b351
ismail kaya <tor232191@hotmail.com>
2021-11-02 23:12:12 +0100

    Create test.java
    desktopTest

---
+++ src/part9/classInheritance/ABC/test.java
@@ -0,0 +1,11 @@
+package part9.classInheritance.ABC;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		System.out.println("deneme");
+
+	}
+
+}
